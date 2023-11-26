7527921e5c301191503953c235a03692471129ff
LassazVegaz <lasindunuwanga@gmail.com>
2023-11-26 09:55:54 +0530

    sectionized

---
+++ src/app/components/Sections/SectionOne.tsx
@@ -0,0 +1,22 @@
+import { Box, Typography } from "@mui/material";
+import AnimateFillingTexts from "../AnimateFillingTexts";
+
+const SectionOne = () => (
+  <Box height="100vh" display="flow-root">
+    <Typography variant="h3" fontWeight={900} ml="10vw" mt="30vh">
+      Lasindu Nuwanga Weerasinghe
+    </Typography>
+    <Typography variant="h4" ml="10vw" mt="5vh">
+      Senior Software Engineer
+    </Typography>
+    <AnimateFillingTexts
+      texts={[
+        "Web Apps Developer",
+        "Mobile Apps Developer",
+        "Desktop Apps Developer",
+      ]}
+    />
+  </Box>
+);
+
+export default SectionOne;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-import { Box, Typography } from "@mui/material";
-import AnimateFillingTexts from "./components/AnimateFillingTexts";
+import { SectionOne } from "./components/Sections";
 
 export default function Home() {
-  return (
-    <Box height="100vh" display="flow-root">
-      <Typography variant="h3" fontWeight={900} ml="10vw" mt="30vh">
-        Lasindu Nuwanga Weerasinghe
-      </Typography>
-      <Typography variant="h4" ml="10vw" mt="5vh">
-        Senior Software Engineer
-      </Typography>
-      <AnimateFillingTexts
-        texts={[
-          "Web Apps Developer",
-          "Mobile Apps Developer",
-          "Desktop Apps Developer",
-        ]}
-      />
-    </Box>
-  );
+  return <SectionOne />;
 }
