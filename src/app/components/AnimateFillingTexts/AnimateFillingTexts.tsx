26d88b7f9dded84cf78d93c6ae350f20861b9721
LassazVegaz <lasindunuwanga@gmail.com>
2023-11-30 11:24:50 +0530

    ani text height fix

--- src/app/components/AnimateFillingTexts/AnimateFillingTexts.tsx
+++ src/app/components/AnimateFillingTexts/AnimateFillingTexts.tsx
@@ -39,7 +39,7 @@ const AnimateFillingTexts = (props: AnimateFillingTextProps) => {
   }, [props.texts, text.length]);
 
   return (
-    <Typography variant="h5" ml="10vw" mt="5vh">
+    <Typography variant="h5" ml="10vw" mt="5vh" height={70}>
       {text}
     </Typography>
   );
