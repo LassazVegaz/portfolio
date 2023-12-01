e9a3bf9d04f8d000c31c9179b42e879796231b2b
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-01 10:44:31 +0530

    good UI to timeline

--- src/app/components/Sections/SectionTimeline.tsx
+++ src/app/components/Sections/SectionTimeline.tsx
@@ -10,20 +10,36 @@ import {
 } from "@mui/lab";
 import { Typography } from "@mui/material";
 import timelineData from "@/app/helpers/timeline-data.helper";
+import styles from "./SectionTimeline.module.scss";
 
 const SectionTimeline = () => {
   return (
     <Timeline>
       {timelineData.map((data, index) => (
-        <TimelineItem key={index}>
-          <TimelineOppositeContent>{data.range}</TimelineOppositeContent>
+        <TimelineItem key={index} className={styles["timeline-item"]}>
+          <TimelineOppositeContent
+            color="#000000"
+            className={styles["onhover-opacity"]}
+            sx={{ opacity: 0.4 }}
+          >
+            {data.range}
+          </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot
+              color="primary"
+              sx={{ p: 0.8 }}
+              className={styles["onhover-glow"]}
+            />
             <TimelineConnector />
           </TimelineSeparator>
-          <TimelineContent sx={{ px: 2, py: 0 }}>
+          <TimelineContent
+            sx={{ px: 2, pt: 0, pb: 4 }}
+            className={styles["onhover-enlarge"]}
+          >
             <Typography variant="h5">{data.title}</Typography>
-            <Typography variant="h6">{data.company}</Typography>
+            <Typography variant="h6" mb={1}>
+              {data.company}
+            </Typography>
             <ul>
               {data.points.map((point, index) => (
                 <li key={index}>
