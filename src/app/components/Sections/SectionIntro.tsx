db5ccfc6dc528c8f810797472dd81b2229387228
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-20 10:25:39 +0530

    unused div removed

--- src/app/components/Sections/SectionIntro.tsx
+++ src/app/components/Sections/SectionIntro.tsx
@@ -1,23 +1,21 @@
 import { AnimateFillingTexts } from "../AnimateFillingTexts";
 
 const SectionIntro = () => (
-  <section id="intro" className="h-screen px-[10vw] grid grid-rows-[1fr,auto]">
-    <div>
-      <h1 className="font-extrabold mt-[30vh] text-4xl sm:text-6xl">
-        Lasindu Nuwanga Weerasinghe
-      </h1>
-      <h4 className="font-semibold text-xl sm:text-3xl mt-[4vh]">
-        Senior Software Engineer
-      </h4>
+  <section id="intro" className="h-screen px-[10vw] flow-root">
+    <h1 className="font-extrabold mt-[30vh] text-4xl sm:text-6xl">
+      Lasindu Nuwanga Weerasinghe
+    </h1>
+    <h4 className="font-semibold text-xl sm:text-3xl mt-[4vh]">
+      Senior Software Engineer
+    </h4>
 
-      <AnimateFillingTexts
-        texts={[
-          "Web Apps Developer",
-          "Mobile Apps Developer",
-          "Desktop Apps Developer",
-        ]}
-      />
-    </div>
+    <AnimateFillingTexts
+      texts={[
+        "Web Apps Developer",
+        "Mobile Apps Developer",
+        "Desktop Apps Developer",
+      ]}
+    />
   </section>
 );
 
