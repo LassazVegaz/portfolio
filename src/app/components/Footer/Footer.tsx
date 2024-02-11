9d05218cb84706f0067316e9f68c3d8b5ba63481
LassazVegaz <lasindunuwanga@gmail.com>
2024-02-11 13:09:07 +0530

    footer dark mode support

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="py-10 text-center border-t-2 border-gray-300">
+    <footer className="py-10 text-center border-t-2 border-gray-300 dark:border-gray-500 dark:text-slate-400">
       <p>&copy; {year} Lasindu Nuwanga Weerasinghe</p>
     </footer>
   );
