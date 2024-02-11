fb4a4bc7519fd656ca818c13ca6928f39bd4d5ad
LassazVegaz <lasindunuwanga@gmail.com>
2024-02-11 12:40:45 +0530

    body dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={`${roboto.className} dark:bg-black`}>{children}</body>
     </html>
   );
 }
