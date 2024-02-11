a752d0ab4bf403da4ee71b7074c25f726b53f376
LassazVegaz <lasindunuwanga@gmail.com>
2024-02-11 12:40:56 +0530

    first sec dark mode

--- src/app/components/Sections/SectionIntro.tsx
+++ src/app/components/Sections/SectionIntro.tsx
@@ -2,10 +2,10 @@ import { AnimateFillingTexts } from "../AnimateFillingTexts";
 
 const SectionIntro = () => (
   <section id="intro" className="h-screen px-[10vw] flow-root">
-    <h1 className="font-extrabold mt-[30vh] text-4xl sm:text-6xl">
+    <h1 className="font-extrabold mt-[30vh] text-4xl sm:text-6xl dark:text-white">
       Lasindu Nuwanga Weerasinghe
     </h1>
-    <h4 className="font-semibold text-xl sm:text-3xl mt-[4vh]">
+    <h4 className="font-semibold text-xl sm:text-3xl mt-[4vh] dark:text-slate-100">
       Senior Software Engineer
     </h4>
 
