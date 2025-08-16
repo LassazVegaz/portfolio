c95d8f3c07d37f2b846b1a27e836525956769efd
LassazVegaz <lasindunuwanga@gmail.com>
2025-08-16 13:53:24 +0800

    career timeline UI made with tailwind

--- src/app/components/Sections/SectionTimeline.tsx
+++ src/app/components/Sections/SectionTimeline.tsx
@@ -1,39 +1,27 @@
-"use client";
-import {
-  TimelineItem,
-  TimelineSeparator,
-  TimelineDot,
-  TimelineConnector,
-  TimelineContent,
-  Timeline,
-  TimelineOppositeContent,
-  timelineOppositeContentClasses,
-} from "@mui/lab";
 import timelineData, {
   TimelinePiece,
 } from "@/app/helpers/timeline-data.helper";
 import styles from "./SectionTimeline.module.scss";
 
-const TimelineItemBla = ({ data }: { data: TimelinePiece }) => (
-  <TimelineItem className={styles["timeline-item"]}>
-    <TimelineOppositeContent
-      className={`text-sm lg:text-base text-center md:text-right ${styles["onhover-date-range"]}`}
-      sx={{ opacity: 0.4 }}
+const TimelineItem = ({ data }: { data: TimelinePiece }) => (
+  <div
+    className={`${styles["timeline-item"]} grid grid-cols-[auto_auto_1fr] gap-4 w-[700px]`}
+  >
+    <div
+      className={`text-sm lg:text-base text-center md:text-right ${styles["onhover-date-range"]} opacity-40 pt-1`}
     >
       {data.range}
-    </TimelineOppositeContent>
-    <TimelineSeparator>
-      <TimelineDot
+    </div>
+    <div className="grid grid-rows-[auto_1fr] gap-2 justify-items-center pt-3">
+      <span
         color="primary"
-        sx={{ p: 0.8 }}
-        className={styles["onhover-glow"]}
+        className={`${styles["onhover-glow"]} w-4 h-4 inline-block bg-blue-600 rounded-full`}
       />
-      <TimelineConnector className={styles["onhover-glow-ani"]} />
-    </TimelineSeparator>
-    <TimelineContent
-      sx={{ px: 2, pt: 0, pb: 4 }}
-      className={styles["onhover-enlarge"]}
-    >
+      <span
+        className={`${styles["onhover-glow-ani"]}  w-0.5 inline-block bg-white opacity-70 rounded`}
+      />
+    </div>
+    <div className={`${styles["onhover-enlarge"]} pb-5`}>
       <h5 className="text-2xl dark:text-slate-50">{data.title}</h5>
       <h6 className="text-xl mb-5 dark:text-slate-100">{data.company}</h6>
       <ul className="dark:text-slate-300">
@@ -43,26 +31,18 @@ const TimelineItemBla = ({ data }: { data: TimelinePiece }) => (
           </li>
         ))}
       </ul>
-    </TimelineContent>
-  </TimelineItem>
+    </div>
+  </div>
 );
 
 const SectionTimeline = () => (
   <section className="py-5 mt-28" id="career-timeline">
     <h4 className="text-4xl text-center mb-8">Career Timeline</h4>
-    <Timeline
-      sx={(theme) => ({
-        [theme.breakpoints.down("md")]: {
-          [`& .${timelineOppositeContentClasses.root}`]: {
-            flex: 0.2,
-          },
-        },
-      })}
-    >
+    <div className="flex flex-col items-center gap-1">
       {timelineData.map((data, index) => (
-        <TimelineItemBla key={index} data={data} />
+        <TimelineItem key={index} data={data} />
       ))}
-    </Timeline>
+    </div>
   </section>
 );
 
