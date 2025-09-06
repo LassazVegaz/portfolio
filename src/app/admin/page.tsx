d1de4c59f0dae071c931e3055d79ac3daa0a3097
LassazVegaz <lasindunuwanga@gmail.com>
2025-09-06 23:08:03 +0800

    mobile first development

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -140,11 +140,13 @@ export default function AdminPage() {
   }, []);
 
   return (
-    <div className="px-20 py-2">
-      <h1 className="text-3xl font-bold text-center">Stocks Calculator</h1>
+    <div className="md:px-20 py-4 md:py-2">
+      <h1 className="text-2xl md:text-3xl font-bold text-center">
+        Stocks Calculator
+      </h1>
 
-      <div className="flex justify-center">
-        <div className="mt-14 w-[400px] grid grid-cols-2 gap-y-2 items-center">
+      <div className="mt-14 flex justify-center">
+        <div className="grid grid-cols-2 gap-y-2 items-center max-w-[400px] w-full">
           <Label>Unit buying price</Label>
           <TextBox
             name="unitBuyingPrice"
