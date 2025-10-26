2ba2a9faf3303412934148f96f3c8dc5b9016151
LassazVegaz <lasindunuwanga@gmail.com>
2025-10-26 16:03:48 +0800

    functional login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,12 +1,20 @@
 import PageContainer from "@/components/PageContainer";
 import Header1 from "@/components/Header1";
+import { loginAction } from "./actions";
+import LoginButton from "./components/LoginButton";
+import routingService from "@/services/routing-service";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  await routingService.authorizedRedirection();
+
   return (
     <PageContainer className="grid grid-rows-[auto_1fr] items-center h-screen">
       <Header1>Access Admin Part</Header1>
 
-      <form className="flex flex-col gap-4 max-w-sm mx-auto w-full">
+      <form
+        className="flex flex-col gap-4 max-w-sm mx-auto w-full"
+        action={loginAction}
+      >
         <p className="text-gray-600 text-center">
           This is a protected admin page. Only authorized users can access this
           section.
@@ -15,19 +23,16 @@ export default function LoginPage() {
         <input
           type="text"
           name="username"
+          required
           className="p-2 border border-gray-300 rounded"
         />
         <input
           type="password"
           name="password"
+          required
           className="p-2 border border-gray-300 rounded"
         />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white p-2 rounded cursor-pointer"
-        >
-          Login
-        </button>
+        <LoginButton />
       </form>
     </PageContainer>
   );
