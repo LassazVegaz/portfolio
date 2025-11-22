cdc13a3ca5cc93da728a853432f570df7b70fe37
LassazVegaz <lasindunuwanga@gmail.com>
2025-11-22 12:28:48 +0800

    rent rooms floating add btn

---
+++ src/app/admin/rent-rooms/page.tsx
@@ -0,0 +1,18 @@
+import Header1 from "@/components/Header1";
+import PageContainer from "@/components/PageContainer";
+import Link from "next/link";
+
+export default function RentRoomsPage() {
+  return (
+    <PageContainer>
+      <Header1>Rent Rooms</Header1>
+
+      <Link
+        className="inline-block text-center p-2 border border-gray-300 w-12 h-12 rounded-full text-2xl absolute bottom-8 right-8"
+        href="/admin/rent-rooms/new-room"
+      >
+        +
+      </Link>
+    </PageContainer>
+  );
+}
