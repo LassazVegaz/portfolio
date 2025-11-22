83f78c88d3611222aa440d8552302c7655679ae9
LassazVegaz <lasindunuwanga@gmail.com>
2025-11-22 20:49:17 +0800

    basic add room page

---
+++ src/app/admin/rent-rooms/new-room/page.tsx
@@ -0,0 +1,35 @@
+import Header1 from "@/components/Header1";
+import PageContainer from "@/components/PageContainer";
+
+export default function NewRoomPage() {
+  return (
+    <PageContainer className="grid grid-rows-[auto_1fr] h-screen">
+      <Header1>Add New Room</Header1>
+
+      <form className="space-y-4 mt-6 mx-5 grid grid-rows-[1fr_auto]">
+        <div>
+          <label htmlFor="roomLink">Room link</label>
+          <input
+            type="text"
+            id="roomLink"
+            name="roomLink"
+            placeholder="Enter room link"
+            className="border border-gray-300 p-2 w-full rounded"
+          />
+        </div>
+
+        <div className="flex justify-end space-x-4">
+          <button className="px-4 py-2 rounded border border-gray-300 min-w-24">
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded min-w-24"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </PageContainer>
+  );
+}
