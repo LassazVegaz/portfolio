cc3717acee40e5cb516722fdc8ec860592303967
LassazVegaz <lasindunuwanga@gmail.com>
2025-11-22 20:52:28 +0800

    new room is in [id] folder

--- src/app/admin/rent-rooms/page.tsx
+++ src/app/admin/rent-rooms/page.tsx
@@ -9,7 +9,7 @@ export default function RentRoomsPage() {
 
       <Link
         className="inline-block text-center p-2 border border-gray-300 w-12 h-12 rounded-full text-2xl absolute bottom-8 right-8"
-        href="/admin/rent-rooms/new-room"
+        href="/admin/rent-rooms/new"
       >
         +
       </Link>
