2ee14d3b4705ab6828722da84aca4ae406392b05
LassazVegaz <lasindunuwanga@gmail.com>
2025-11-24 20:09:36 +0800

    AppCard accepts typed routes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,13 +3,11 @@ import Header1 from "@/components/Header1";
 import Link from "next/link";
 import routingService from "@/services/routing-service";
 
-const AppCard = (props: { href: string; children: React.ReactNode }) => (
+const AppCard: typeof Link = (props) => (
   <Link
-    href={props.href}
+    {...props}
     className="bg-black border-2 border-white rounded p-1 w-32 h-32 flex items-center justify-center text-center md:text-lg font-semibold hover:bg-white hover:text-black transition-all duration-1000 odd:justify-self-end"
-  >
-    {props.children}
-  </Link>
+  />
 );
 
 export default async function AdminPage() {
