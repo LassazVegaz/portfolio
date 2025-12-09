3a95aa1846f32370ab8ae20f176197df750b1b36
LassazVegaz <lasindunuwanga@gmail.com>
2025-12-09 23:14:18 +0800

    create the room in DB

--- src/app/admin/rent-rooms/[id]/actions.ts
+++ src/app/admin/rent-rooms/[id]/actions.ts
@@ -1,10 +1,13 @@
 "use server";
 import cs from "@/services/cs-scrapping-service";
+import roomsRepo from "@/repositories/renting-rooms-repository";
 
 export const fetchData = async (formData: FormData) => {
   const url = formData.get("roomLink");
   if (typeof url !== "string") throw new Error("roomLink is not a string");
 
   await cs.loadData(url);
-  console.log(cs.getData());
+  const data = cs.getData();
+
+  await roomsRepo.create(data);
 };
