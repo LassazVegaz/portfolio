60b3a8e7a7ec6b2f7608b32177366366e217b2ea
LassazVegaz <lasindunuwanga@gmail.com>
2026-01-24 15:52:18 +0800

    prisma model: category

--- prisma.config.ts
+++ prisma.config.ts
@@ -14,9 +14,16 @@ config({
     .filter((f) => fs.existsSync(f)),
 });
 
+const dbUrl = process.env.DATABASE_URL;
+if (!dbUrl)
+  throw new Error("DATABASE_URL is not set in the environment variables.");
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+  },
+  datasource: {
+    url: dbUrl,
   },
 });
