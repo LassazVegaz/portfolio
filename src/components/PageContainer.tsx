f441a489196080542fb3a80f12c58c8b8f4ad3c5
LassazVegaz <lasindunuwanga@gmail.com>
2026-01-24 13:28:27 +0800

    more reuasable components

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -2,5 +2,5 @@
  * The main container of the page
  */
 export default function PageContainer(props: React.ComponentProps<"div">) {
-  return <div {...props} className={`py-4 ${props.className}`} />;
+  return <div {...props} className={`py-4 px-6 ${props.className}`} />;
 }
