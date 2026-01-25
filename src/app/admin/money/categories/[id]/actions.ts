bca960264bfbbeb54796d86e17cfe7f19c716843
LassazVegaz <lasindunuwanga@gmail.com>
2026-01-25 12:17:10 +0800

    a basic action for categories

---
+++ src/app/admin/money/categories/[id]/actions.ts
@@ -0,0 +1,35 @@
+"use server";
+import categoriesService from "@/services/categories.service";
+import { redirect } from "next/navigation";
+
+type FormDataDto = {
+  id: string;
+  name: string;
+  parentId: string | null;
+  save?: string;
+  delete?: string;
+};
+
+export async function formAction(prevState: null, form: FormData) {
+  const data = Object.fromEntries(form.entries()) as FormDataDto;
+
+  data.parentId = data.parentId || null;
+
+  if (data.save && data.id === "new") {
+    const created = await categoriesService.createCategory({
+      name: data.name,
+      parentId: data.parentId,
+    });
+    redirect(`/admin/money/${"categories/" + created.id}`);
+  } else if (data.save) {
+    await categoriesService.updateCategory(data.id, {
+      name: data.name,
+      parentId: data.parentId,
+    });
+  } else if (data.delete) {
+    await categoriesService.deleteCategory(data.id);
+    redirect("/admin/money/categories");
+  }
+
+  return null;
+}
