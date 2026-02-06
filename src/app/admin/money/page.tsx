8d2a52b636cc8772e947e163bc9e1d722bb2f563
LassazVegaz <lasindunuwanga@gmail.com>
2026-02-06 15:49:06 +0800

    money apps

--- src/app/admin/money/page.tsx
+++ src/app/admin/money/page.tsx
@@ -1,10 +1,16 @@
+import AppCard from "@/components/AppCard";
 import Header1 from "@/components/Header1";
 import PageContainer from "@/components/PageContainer";
 
 export default function MoneyPage() {
   return (
-    <PageContainer>
+    <PageContainer className="grid grid-rows-[auto_1fr] items-center h-screen">
       <Header1>Money</Header1>
+
+      <div className="grid grid-cols-2 gap-4 items-center">
+        <AppCard href="/admin/money/transactions">Transactions</AppCard>
+        <AppCard href="/admin/money/categories">Categories</AppCard>
+      </div>
     </PageContainer>
   );
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,7 @@
 import PageContainer from "@/components/PageContainer";
 import Header1 from "@/components/Header1";
-import Link from "next/link";
 import routingService from "@/services/routing-service";
-
-const AppCard: typeof Link = (props) => (
-  <Link
-    {...props}
-    className="bg-black border-2 border-white rounded p-1 w-32 h-32 flex items-center justify-center text-center md:text-lg font-semibold hover:bg-white hover:text-black transition-all duration-1000 odd:justify-self-end"
-  />
-);
+import AppCard from "@/components/AppCard";
 
 export default async function AdminPage() {
   await routingService.unauthorizedRedirection();
@@ -20,7 +13,7 @@ export default async function AdminPage() {
       <div className="grid grid-cols-2 gap-4 items-center">
         <AppCard href="/admin/stocks-calculator">Stocks Calculator</AppCard>
         <AppCard href="/admin/rent-rooms">Rent Rooms</AppCard>
-        <AppCard href="/admin">Comming Soon</AppCard>
+        <AppCard href="/admin/money">Money</AppCard>
         <AppCard href="/admin">Comming Soon</AppCard>
       </div>
     </PageContainer>
