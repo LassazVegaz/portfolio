efb006c30082d6c3bc10e0eae0ed171d4e3b59db
LassazVegaz <lasindunuwanga@gmail.com>
2026-06-16 16:23:29 +0800

    social media links updated ⬆️ damn! i don't even have FB anymore 🤣

--- src/app/components/Icons/Icons.tsx
+++ src/app/components/Icons/Icons.tsx
@@ -1,6 +1,4 @@
 import SocialIcon from "./SocialIcon";
-import fbIcon from "@/assets/icons/facebook-f.svg";
-import whatsappIcon from "@/assets/icons/whatsapp.svg";
 import githubIcon from "@/assets/icons/github.svg";
 import linkedinIcon from "@/assets/icons/linkedin-in.svg";
 import twitterIcon from "@/assets/icons/x-twitter.svg";
@@ -8,11 +6,13 @@ import discordIcon from "@/assets/icons/discord.svg";
 
 const Icons = (props: React.ComponentProps<"div">) => (
   <div {...props}>
-    <SocialIcon
+    {/*
+    My girlfriend probably doesnt want me to publish my number 🤣
+    <SocialIcon 
       src={whatsappIcon}
       alt="WhatsApp"
       link="https://wa.me/+94774556607"
-    />
+    /> */}
     <SocialIcon
       src={githubIcon}
       alt="GitHub"
@@ -21,7 +21,7 @@ const Icons = (props: React.ComponentProps<"div">) => (
     <SocialIcon
       src={linkedinIcon}
       alt="LinkedIn"
-      link="https://www.linkedin.com/in/lasindu-w-abb08413a/"
+      link="https://www.linkedin.com/in/lasindu-weerasinghe"
     />
     <SocialIcon
       src={twitterIcon}
@@ -32,11 +32,6 @@ const Icons = (props: React.ComponentProps<"div">) => (
       src={discordIcon}
       alt="Discord"
       link="https://discordapp.com/users/7715"
-    />
-    <SocialIcon
-      src={fbIcon}
-      alt="Facebook"
-      link="https://web.facebook.com/lasindu.nuwanga.5"
     />
   </div>
 );
