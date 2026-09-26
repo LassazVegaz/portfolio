896f71ee479e88a54409f2920989e3ee1392bf1f
LassazVegaz <lasindunuwanga@gmail.com>
2026-09-26 15:51:06 +0800

    data scroll behaviour smooth for nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body>
         {children}
         <Analytics />
