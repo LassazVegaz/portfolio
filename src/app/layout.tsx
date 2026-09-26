40b38aa965cba906ce6c6c2f4a682b18f3f63ab9
LassazVegaz <lasindunuwanga@gmail.com>
2026-09-26 15:51:18 +0800

    readonly props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,9 @@ export const viewport: Viewport = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <body>
